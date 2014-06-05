8f10709b30abde091d4d52a97d478b915a924a81
dagobailon <dago@codeforprogress.org>
2014-06-05 15:03:55 -0400

    New Basic HTML files

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-
 from app import app
 from flask import render_template
 
 @app.route('/')
 def index():
-	return "Hello World!"
+	return render_template('index.html')
+
+@app.route('/sign_up')
+def sign_up():
+	return render_template('sign_up.html')
+
+@app.route('/topics')
+def topics():
+	return render_template('topics.html')
+
