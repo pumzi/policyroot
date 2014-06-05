6cb0d74a79d1ed6bc61a6016f7b28e7e5f63bca6
dagobailon <dago@codeforprogress.org>
2014-06-05 16:30:20 -0400

    added topics basic html

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 from app import app
 from flask import render_template
 
@@ -15,27 +14,42 @@ def sign_up():
 def topics():
 	return render_template('topics.html')
 
-=======
+@app.route('/Undocuqueer')
+def Undocuqueer():
+	return render_template('Undocuqueer.html')
 
-from app import app, db
-from flask import Flask, render_template, redirect
-from models import Post, User
-from forms import NewUserForm
+@app.route('/pathway')
+def pathway():
+	return render_template('pathway.html')
 
-@app.route('/')
-def index():
-	users = User.query.all()
-	posts = Post.query.all()
-	return render_template('index.html', users = users, posts = posts )
+@app.route('/know_your_rights')
+def know_your_rights():
+	return render_template('know_your_rights.html')
+@app.route('/colonialism')
+def colonialism():
+	return render_template('colonialism.html')
 
-@app.route('/add_user', methods = ['GET', 'POST'])
-def add_user():
-	form = NewUserForm()
-	if form.validate_on_submit():
-		user = User()
-		form.populate_obj(user)
-		db.session.add(user)
-		db.session.commit()
-		return redirect('/')
-	return render_template("add_user.html", form = form)
->>>>>>> bbac8ee1b436ab8737ec10acf5da017000df2890
+#=======
+
+#from app import app, db
+#from flask import Flask, render_template, redirect
+#from models import Post, User
+#from forms import NewUserForm
+
+#@app.route('/')
+#def index():
+#	users = User.query.all()
+#	posts = Post.query.all()
+#	return render_template('index.html', users = users, posts = posts )
+
+#@app.route('/add_user', methods = ['GET', 'POST'])
+#def add_user():
+#	form = NewUserForm()
+#	if form.validate_on_submit():
+#		user = User()
+#		form.populate_obj(user)
+#		db.session.add(user)
+#		db.session.commit()
+#		return redirect('/')
+#	return render_template("add_user.html", form = form)
+#>>>>>>> bbac8ee1b436ab8737ec10acf5da017000df2890
