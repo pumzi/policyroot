c0fcb4a3f27dced3bb0256503081a0cc4d35b964
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-06-06 10:40:35 -0400

    cleaned up the views file for merge

--- app/views.py
+++ app/views.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-from app import app, db#, lm
+from app import app, db, lm
 from flask import render_template, redirect
-#from flask.ext.login import login_user 
+from flask.ext.login import login_user 
 from models import Post, User
-#from forms import NewUserForm, LoginForm
+from forms import NewUserForm, LoginForm
 
 @app.route('/')
 def index():
-  	#form = LoginForm()
-	#if form.validate_on_submit():
-	#	user = form.get_user()
-	#	login_user(user)
-	#	return redirect('/')
-	return render_template('index.html')#, form = form)
+  	form = LoginForm()
+	if form.validate_on_submit():
+		user = form.get_user()
+		login_user(user)
+		return redirect('/')
+	return render_template('index.html', form = form)
 
 @app.route('/sign_up', methods = ['GET', 'POST'])
 def sign_up():
-	#form = NewUserForm()
-	#if form.validate_on_submit():
-	#	user = User()
-	#	form.populate_obj(user)
-	#	db.session.add(user)
-	#	db.session.commit()
-	#	return redirect('/')
+	form = NewUserForm()
+	if form.validate_on_submit():
+		user = User()
+		form.populate_obj(user)
+		db.session.add(user)
+		db.session.commit()
+		return redirect('/')
 	return render_template('sign_up.html')
 
 @app.route('/topics', methods = ['GET', 'POST'])
