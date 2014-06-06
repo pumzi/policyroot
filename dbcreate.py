98b890ada43002ddb4629d869dd2a55b86831951
Selina Musuta <selina@codeforprogress.org>
2014-06-05 20:46:16 -0400

    sample data and our database

---
+++ dbcreate.py
@@ -0,0 +1,8 @@
+
+# Creates the database based on the models we've defined in models.py
+
+
+from config import SQLALCHEMY_DATABASE_URI
+
+from app import db 
+db.create_all()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/bobbyjoesmith/gitrepos/policyroot/venv"
+set -gx VIRTUAL_ENV "/home/selinamusuta/gitrepos/project_cfp/policyroot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
