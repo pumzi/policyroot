a6878143bfe31c9af7c886f45a3541b110ba937b
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2014-06-05 20:35:06 -0400

    updated html for the landing page

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dagobailon/github_repos/policyroot/venv"
+set -gx VIRTUAL_ENV "/home/bobbyjoesmith/gitrepos/policyroot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
