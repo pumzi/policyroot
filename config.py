7784243c8fb079b271f12aba80bf7da5b210e1f4
Selina Musuta <selina@codeforprogress.org>
2014-06-06 09:55:08 -0400

    resolving changes

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/policyroot'
-=======
+#=======
 from flask.ext.login import LoginManager
 from config import basedir
 
 lm = LoginManager()
 
-#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/swamperdb'
->>>>>>> f1f46c835d9d407cd475f1f3004fd0907d1f2062
+
+#>>>>>>> f1f46c835d9d407cd475f1f3004fd0907d1f2062
 #We need the login for the postres sql 
 
 CSRF_ENABLED = True #This is to prevent CSRF attacks - so that all of our data comes from our forms
